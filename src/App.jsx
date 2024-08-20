8af5a6f35a3db4e3944a0d9766b1ea8181adac7f
unknown <matijailicmatke@gmail.com>
2024-08-20 15:47:38 +0200

    New React project, created home page,navbar, a href to doctors page and that page is gonna display all veterinarians axios API to spring boot backend, doctors are in mysql database.

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/home/Home";
+import VeterinarianListing from "./components/veterinarian/VeterinarianListing";
+import {
+  Route,
+  Router,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import RootLayout from "./components/layout/RootLayout";
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout />}>        
+        <Route index element={<Home />} />
+        <Route path='/doctors' element={<VeterinarianListing />} />
+      </Route>
+    )
+  );
+  return (
+    <main className=''>
+      <RouterProvider router={router} />
+    </main>
+  );
+}
+
+export default App;
